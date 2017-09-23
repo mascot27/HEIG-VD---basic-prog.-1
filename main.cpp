3ba7f195fc2668a7c56e378289af141bd9484f88
czeller-heig <>
2017-09-23 12:57:28 +0200

    a

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: czeller-heig
+ *
+ * Created on September 22, 2017, 10:55 PM
+ */
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+    
+    cout << "Hello Coco Channel <3" << endl;
+    
+    return 0;
+}
